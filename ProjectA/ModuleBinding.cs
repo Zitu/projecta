6b667cb9128f7c91f1db6288f2bfcf4fe531eab2
Zitu <muntasirzitu@yahoo.com>
2015-01-11 17:17:28 +0600

    Second attempt of DIP on Task2

--- ProjectA/ModuleBinding.cs
+++ ProjectA/ModuleBinding.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Ninject;
 using Ninject.Modules;
 using Package1;
+using Package2;
+using Package3;
 
 namespace ProjectA
 {
@@ -14,6 +16,18 @@ namespace ProjectA
         public override void Load()
         {
             Bind<IInterfaceSomething>().To<ClassM>();
+            Bind<IA>().To<ClassA>();
+            Bind<IK>().To<ClassK>();
+            Bind<IL>().To<ClassL>();
+            Bind<IT>().To<ClassT>();
+            Bind<IX>().To<ClassX>();
+            Bind<IJ>().To<ClassJ>();
+            Bind<IP>().To<ClassP>();
+            Bind<IG>().To<ClassG>();
+            Bind<IR>().To<ClassR>();
+            Bind<IS>().To<ClassS>();
+            Bind<IY>().To<ClassY>();
+
         }
     }
 }
